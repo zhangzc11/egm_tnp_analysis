d59a1e024e1a2c7cf8d2476add8899a07f09bb37
livia <livia.soffi@cern.ch>
2018-05-15 10:13:44 +0200

    Running on 15% of MC stat

--- etc/inputs/tnpSampleDef.py
+++ etc/inputs/tnpSampleDef.py
@@ -6,7 +6,7 @@ from libPython.tnpClassUtils import tnpSample
 #eosDirREC = 'eos/cms/store/group/phys_egamma/tnp/80X/RecoSF/RECOSFs_2016/'
 #eosWinter17 = 'eos/cms/store/group/phys_egamma/tnp/80X/PhoEleIDs/Moriond17_v1/'
 eosMoriond18 = '/eos/cms/store/group/phys_egamma/soffi/TnP/ntuples_01292018/Moriond18_V1/'
-
+eos2018Data_V1 = '/eos/cms/store/group/phys_egamma/soffi/TnP/ntuples_05122018/2018Data_V1/'
 Moriond18_94X = {
     ### MiniAOD TnP for IDs scale factors
     'DY_madgraph'          : tnpSample('DY_madgraph',
@@ -26,3 +26,24 @@ Moriond18_94X = {
     'data_Run2017F' : tnpSample('data_Run2017F' , eosMoriond18 + 'data/TnPTree_17Nov2017_RunF.root' , lumi = 13.96 ),
 
     }
+
+Data2018_10_1_X = {
+
+#    'DY_madgraph'          : tnpSample('DY_madgraph',
+#                                       eosMoriond18 + 'mc/TnPTree_DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_all.root',
+#                                       isMC = True, nEvts =  -1 ),
+    'DY_madgraph_2018_15p' : tnpSample('DY_madgraph_2018_15p',
+                                       eos2018Data_V1 + 'mc/TnPTree_DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_15p.root',
+                                       isMC = True, nEvts =  -1 ),
+#    'DY_amcatnlo_2018' : tnpSample('DY_amcatnlo_Moriond18',
+#                                       eosMoriond18 + 'mc/TnPTree_DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8_all.root',
+#                                       isMC = True, nEvts =  -1 ),
+
+    'data_2018_RunA' : tnpSample('data_2018_RunA' , eos2018Data_V1 + 'data/TnPTree_Prompt2018_RunA.root' , lumi = 5.9 ),
+
+
+
+
+
+
+}
