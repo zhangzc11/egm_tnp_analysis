e4588eee6e400f9f433ff0908f8689f259284f49
livia <livia.soffi@cern.ch>
2018-09-06 09:13:37 +0200

    Adding 2018 102X

--- etc/inputs/tnpSampleDef.py
+++ etc/inputs/tnpSampleDef.py
@@ -6,43 +6,41 @@ from libPython.tnpClassUtils import tnpSample
 #eosDirREC = 'eos/cms/store/group/phys_egamma/tnp/80X/RecoSF/RECOSFs_2016/'
 #eosWinter17 = 'eos/cms/store/group/phys_egamma/tnp/80X/PhoEleIDs/Moriond17_v1/'
 eosMoriond18 = '/eos/cms/store/group/phys_egamma/soffi/TnP/ntuples_01292018/Moriond18_V1/'
-#eos2018Data_V1 = '/eos/cms/store/group/phys_egamma/soffi/TnP/ntuples_05122018/2018Data_V1/'
-eos2018Data_V3 = '/eos/cms/store/group/phys_egamma/soffi/TnP/ntuples_06152018/2018Data_1/'
-Moriond18_94X = {
-    ### MiniAOD TnP for IDs scale factors
-    'DY_madgraph'          : tnpSample('DY_madgraph',
-                                       eosMoriond18 + 'mc/TnPTree_DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_all.root',
-                                       isMC = True, nEvts =  -1 ),
-    'DY_madgraph_Moriond18' : tnpSample('DY_madgraph_Moriond18', 
-                                       eosMoriond18 + 'mc/TnPTree_DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_all.root',
-                                       isMC = True, nEvts =  -1 ),
-    'DY_amcatnlo_Moriond18' : tnpSample('DY_amcatnlo_Moriond18', 
-                                       eosMoriond18 + 'mc/TnPTree_DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8_all.root',
+eos2018Data_102X = '/eos/cms/store/group/phys_egamma/micheli/TnP/ntuples_20180831/2018Data_1/'
+
+Data2018_102X = {
+    ### MiniAOD TnP for IDs scale 
+    'DY_madgraph_part012' : tnpSample('DY_madgraph_part012', 
+                                       eos2018Data_102X + 'mc/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_part012.root',
                                        isMC = True, nEvts =  -1 ),
 
-    'data_Run2017B' : tnpSample('data_Run2017B' , eosMoriond18 + 'data/TnPTree_17Nov2017_RunB.root' , lumi = 4.891 ),
-    'data_Run2017C' : tnpSample('data_Run2017C' , eosMoriond18 + 'data/TnPTree_17Nov2017_RunC.root' , lumi = 9.9 ),
-    'data_Run2017D' : tnpSample('data_Run2017D' , eosMoriond18 + 'data/TnPTree_17Nov2017_RunD.root' , lumi = 4.36 ),
-    'data_Run2017E' : tnpSample('data_Run2017E' , eosMoriond18 + 'data/TnPTree_17Nov2017_RunE.root' , lumi = 9.53 ),
-    'data_Run2017F' : tnpSample('data_Run2017F' , eosMoriond18 + 'data/TnPTree_17Nov2017_RunF.root' , lumi = 13.96 ),
+#    'DY_madgraph_part0' : tnpSample('DY_madgraph_part0', 
+#                                       eos2018Data_102X + 'mc/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_part0.root',
+#                                       isMC = True, nEvts =  -1 ),
+#    'DY_madgraph_part1' : tnpSample('DY_madgraph_part1', 
+#                                       eos2018Data_102X + 'mc/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_part1.root',
+#                                       isMC = True, nEvts =  -1 ),
+#    'DY_madgraph_part2' : tnpSample('DY_madgraph_part2', 
+#                                       eos2018Data_102X + 'mc/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_part2.root',
+#                                       isMC = True, nEvts =  -1 ),
+    'data_Run2018Av1' : tnpSample('data_Run2018Av1' , eos2018Data_102X + 'data/Prompt2018_RunA_v1.root' , lumi = 20),  
+    'data_Run2018Av2' : tnpSample('data_Run2018Av2' , eos2018Data_102X + 'data/Prompt2018_RunA_v2.root' , lumi = 0),
+    'data_Run2018Av3' : tnpSample('data_Run2018Av3' , eos2018Data_102X + 'data/Prompt2018_RunA_v3.root' , lumi = 0),
+
+    'data_Run2018Bv1' : tnpSample('data_Run2018Bv1' , eos2018Data_102X + 'data/Prompt2018_RunB_v1.root' , lumi = 0),
+    'data_Run2018Bv2' : tnpSample('data_Run2018Bv2' , eos2018Data_102X + 'data/Prompt2018_RunB_v2.root' , lumi = 0),
+
+    'data_Run2018Cv1' : tnpSample('data_Run2018Cv1' , eos2018Data_102X + 'data/Prompt2018_RunC_v1.root' , lumi = 0),
+    'data_Run2018Cv2' : tnpSample('data_Run2018Cv2' , eos2018Data_102X + 'data/Prompt2018_RunC_v2.root' , lumi = 0),
+
+    'data_Run2018Dv2' : tnpSample('data_Run2018Dv2' , eos2018Data_102X + 'data/Prompt2018_RunD_v2.root' , lumi = 0),
+
 
     }
 
-Data2018_10_1_X = {
-
-#    'DY_madgraph'          : tnpSample('DY_madgraph',
-#                                       eosMoriond18 + 'mc/TnPTree_DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_all.root',
-#                                       isMC = True, nEvts =  -1 ),
-    'DY_madgraph_2018_30p' : tnpSample('DY_madgraph_2018_30p',
-                                       eos2018Data_V3 + 'mc/TnP_DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_30pStat_all.root',
-                                       isMC = True, nEvts =  -1 ),
-#    'DY_amcatnlo_2018' : tnpSample('DY_amcatnlo_Moriond18',
-#                                       eosMoriond18 + 'mc/TnPTree_DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8_all.root',
-#                                       isMC = True, nEvts =  -1 ),
-
-    'data_2018_RunA_v123' : tnpSample('data_2018_RunA_v123' , eos2018Data_V3 + 'data/TnP_Prompt2018_RunA_v123_selectedRuns.root' , lumi = 0 ),#FIXME so far all the integrated value of the lumi is assigned to runb, still need to compute individual run lumi
-    'data_2018_RunB_v1' : tnpSample('data_2018_RunB' , eos2018Data_V3 + 'data/TnP_Prompt2018_RunB_v1_all.root' , lumi = 16.59 ),
 
 
+##about lumi: thse ntuples are done with /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/PromptReco/Cert_314472-321221_13TeV_PromptReco_Collisions18_JSON.txt = with recorded luminosity : 31.71 /fb but ~20% are crashed. Also we need to update the single runs lumi
 
-}
+
+ 
